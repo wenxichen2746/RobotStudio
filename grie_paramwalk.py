1415548e626df43ecebd7f9cacc01c9322e35c02
wc2746 <wc2746@columbia.edu>
2022-05-06 20:51:54 -0400

    abc

--- grie_paramwalk.py
+++ grie_paramwalk.py
@@ -5,15 +5,13 @@ import serial
 from math import sin
 import math
 
-
-from grie_motorcontrol import mcontroller
 from grie_functions import *
 
-param=np.load('bestparam_hc0504.npy',allow_pickle='TRUE').item()
-param={'amp': [ 0.64119211, -0.29115442,  0.82053762],\
+#param=np.load('bestparam_hc0504.npy',allow_pickle='TRUE').item()
+param={'amp': [ 0.54119211, -0.29115442,  0.82053762],\
      'phase': [2.2515711 , 0.33513259, 0.70721696],\
-          'devia': [-0.01431187,  0.28189022, -0.01911612],\
-               'womiga': 20}
+          'devia': [-0.01431187,  0.18189022, -0.01911612],\
+               'womiga': 2}
 
 SERIAL_PORT = '/dev/ttyUSB0'
 controller=lewansoul_lx16a.ServoController(
@@ -27,16 +25,16 @@ for i in range(1,9):
     except:
         print(f'Number{i} servo fails to connect') 
 
-tlimit=10
+tlimit=15
 t=0
 config_stand=np.load('captured_frame/config_stand.npy')
 
 while t<tlimit:
     actionarray_m8=param2action_m8(param,t)
-    actionarray=actionarray*1000/(1.33*np.pi)
-    commandarray=actionarray+config_stand
+    actionarray_m8=actionarray_m8*1000/(0.66*np.pi)
+    commandarray=actionarray_m8+config_stand
     for mi in range(1,9):
-        c.servo_list[mi].move(actionarray[mi-1])
+        servo_list[mi].move(commandarray[mi-1])
     t+=0.01
     time.sleep(0.01)
 
